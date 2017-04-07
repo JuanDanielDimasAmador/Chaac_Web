1de17ae2201f2de12d812d06315d650f4a1b2718
JuanDanielDimasAmador <1715110271@utec-tgo.edu.mx>
2017-04-06 21:44:11 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 import web
-        
+
+''' ssl = True #activate ssl certificate 
+
+if ssl == True:
+    from web.wsgiserver import CherryPyWSGIServer
+    
+    Use OpenSSL to generate  keys
+
+    user@host$ openssl genrsa -out server.key 1024
+    user@host$ openssl req -new -key server.key -out server.csr
+    user@host$ openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+
+    
+    CherryPyWSGIServer.ssl_certificate = "ssl/server.crt" 
+    CherryPyWSGIServer.ssl_private_key = "ssl/server.key" '''
+
+
 urls = (
     '/', 'Index',
     '/info', 'Information',
-    '/contact', 'Contact',
-    '/about', 'About',
-    '/login', 'Login',
-    '/signup', 'Signup'
+    '/pieces', 'Pieces'
 )
 
 app = web.application(urls, globals())
 render = web.template.render('templates', base='master')
-"""renderIndex = web.template.render('templates', base='masterIndex')"""
-web.config.debug = False
+web.config.debug = False        
+
 
 class Index:        
     def GET(self):
@@ -22,21 +35,11 @@ class Information:
     def GET(self):
         return render.info()
 
-class Contact:        
+class Pieces:
+    """docstring for Pieces"""
     def GET(self):
-        return render.contact()
-
-class About:        
-    def GET(self):
-        return render.about()
-
-class Login:        
-    def GET(self):
-        return render.login()
-
-class Signup:        
-    def GET(self):
-        return render.signup()
+        return render.pieces()
+        
  
 if __name__ == "__main__":
     app.run()
